b2e9fe202745e935c2f1494459974db33d56a0ac
AdibRamadhan20 <adib2003@students.amikom.ac.id>
2023-04-16 19:48:32 +0700

    first commit

---
+++ TugasPraktikum6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasPraktikum6
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Orang orang1 = new Orang("Adib Ramadhan", 19, "Jl.Swadaya No.180");
+			orang1.TampilanInfo();
+
+			Orang orang2 = new Orang("Adib Ramadhan", 24, "Jl.Ronggolawe No.21");
+			orang2.UpdateInfo();
+
+			Console.ReadKey();
+		}
+	}
+}
